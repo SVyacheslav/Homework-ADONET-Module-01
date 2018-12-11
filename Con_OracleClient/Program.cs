64b1c419ad4a68266e98f9d0351b6db6a5a14a3c
Вячеслав Мастер <smonster777@gmail.com>
2018-12-11 17:49:15 +0600

    Добавьте файлы проекта.

---
+++ Con_OracleClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Microsoft Visual Studio не поддерживает базы данных Oracle
+
+namespace Con_OracleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Con_SqlClient/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace Con_SqlClient
+{
+    class Program
+    {
+      
+        static Program()
+        {
+            
+            // Используется подключение "по умолчанию" определенного в файле App.config. Если используется подключение из строки в коде, нижеследующую строку нужно раскомментировать
+            //connectionString = @"Data Source=SVG-ASUS\SQLEXPRESS;Initial Catalog=Kassa24;integrated security=True;MultipleActiveResultSets=True";
+        }
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            SqlConnection con = new SqlConnection();
+            
+            // получаем строку подключения для App.config. Если используется подключение из строки в коде, нижеследующую строку нужно закомментировать
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            con.ConnectionString = connectionString;
+
+            try
+            {
+                con.Open();
+                Console.WriteLine("Подключение открыто");
+                Console.WriteLine("Свойства подключения:");
+                Console.WriteLine("\tСтрока подключения: {0}", con.ConnectionString);
+                Console.WriteLine("\tБаза данных: {0}", con.Database);
+                Console.WriteLine("\tСервер: {0}", con.DataSource);
+                Console.WriteLine("\tВерсия сервера: {0}", con.ServerVersion);
+                Console.WriteLine("\tСостояние: {0}", con.State);
+                Console.WriteLine("\tВремя ожидания: {0}", con.ConnectionTimeout);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Dispose();
+            }
+            Console.WriteLine("Подключение закрыто...");
+            Console.Read();
+        }
+    }
+}
